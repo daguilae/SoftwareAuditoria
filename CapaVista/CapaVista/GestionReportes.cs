c3faf68c3f1354ec1727503f8a160c636175adcc
eddcas01@gmail.com <eddcas01@gmail.com>
2020-10-02 20:16:17 -0600

    Prototipo No Funcional Gestion de Reportes

---
+++ CapaVista/CapaVista/GestionReportes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class GestionReportes : UserControl
+    {
+        public GestionReportes()
+        {
+            InitializeComponent();
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox8_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
