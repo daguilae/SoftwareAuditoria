624dcac8446ba71fa7f625d074967266e99e0450
imogollonh <52753345+imogollonh@users.noreply.github.com>
2020-10-15 16:45:51 -0600

    Cambios de estandarizacion y agregar navegador

--- GestionRequerimientos/CapaVista/Mantenimientos/Frm_MantenimientoDepartamentos.Designer.cs
+++ GestionRequerimientos/CapaVista/Mantenimientos/Frm_MantenimientoDepartamentos.Designer.cs
@@ -28,141 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_MantenimientoDepartamentos));
             this.lbl_titulo = new System.Windows.Forms.Label();
-            this.Btn_consultar = new System.Windows.Forms.Button();
-            this.Btn_borrar = new System.Windows.Forms.Button();
-            this.Btn_guardar = new System.Windows.Forms.Button();
-            this.Btn_editar = new System.Windows.Forms.Button();
-            this.Btn_ingresar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.navegador2 = new CapaDeDiseno.Navegador();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbl_titulo
             // 
             this.lbl_titulo.AutoSize = true;
             this.lbl_titulo.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_titulo.Location = new System.Drawing.Point(453, 9);
+            this.lbl_titulo.Location = new System.Drawing.Point(3, 0);
             this.lbl_titulo.Name = "lbl_titulo";
             this.lbl_titulo.Size = new System.Drawing.Size(236, 21);
             this.lbl_titulo.TabIndex = 2;
             this.lbl_titulo.Text = "Mantenimiento Departamentos";
             // 
-            // Btn_consultar
+            // tableLayoutPanel1
             // 
-            this.Btn_consultar.BackColor = System.Drawing.Color.SkyBlue;
-            this.Btn_consultar.FlatAppearance.BorderSize = 3;
-            this.Btn_consultar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
-            this.Btn_consultar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_consultar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.Btn_consultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_consultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_consultar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_consultar.Image")));
-            this.Btn_consultar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Btn_consultar.Location = new System.Drawing.Point(689, 66);
-            this.Btn_consultar.Margin = new System.Windows.Forms.Padding(2);
-            this.Btn_consultar.Name = "Btn_consultar";
-            this.Btn_consultar.Size = new System.Drawing.Size(93, 89);
-            this.Btn_consultar.TabIndex = 161;
-            this.Btn_consultar.Text = "Consultar";
-            this.Btn_consultar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Btn_consultar.UseVisualStyleBackColor = false;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lbl_titulo, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.navegador2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(859, 689);
+            this.tableLayoutPanel1.TabIndex = 3;
             // 
-            // Btn_borrar
+            // navegador2
             // 
-            this.Btn_borrar.BackColor = System.Drawing.Color.SkyBlue;
-            this.Btn_borrar.FlatAppearance.BorderSize = 3;
-            this.Btn_borrar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
-            this.Btn_borrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_borrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.Btn_borrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_borrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_borrar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_borrar.Image")));
-            this.Btn_borrar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Btn_borrar.Location = new System.Drawing.Point(605, 66);
-            this.Btn_borrar.Margin = new System.Windows.Forms.Padding(2);
-            this.Btn_borrar.Name = "Btn_borrar";
-            this.Btn_borrar.Size = new System.Drawing.Size(84, 89);
-            this.Btn_borrar.TabIndex = 160;
-            this.Btn_borrar.Text = "Borrar";
-            this.Btn_borrar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Btn_borrar.UseVisualStyleBackColor = false;
-            // 
-            // Btn_guardar
-            // 
-            this.Btn_guardar.BackColor = System.Drawing.Color.SkyBlue;
-            this.Btn_guardar.FlatAppearance.BorderSize = 3;
-            this.Btn_guardar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
-            this.Btn_guardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.Btn_guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_guardar.Image")));
-            this.Btn_guardar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Btn_guardar.Location = new System.Drawing.Point(521, 66);
-            this.Btn_guardar.Margin = new System.Windows.Forms.Padding(2);
-            this.Btn_guardar.Name = "Btn_guardar";
-            this.Btn_guardar.Size = new System.Drawing.Size(84, 89);
-            this.Btn_guardar.TabIndex = 159;
-            this.Btn_guardar.Text = "Guardar";
-            this.Btn_guardar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Btn_guardar.UseVisualStyleBackColor = false;
-            // 
-            // Btn_editar
-            // 
-            this.Btn_editar.BackColor = System.Drawing.Color.SkyBlue;
-            this.Btn_editar.FlatAppearance.BorderSize = 3;
-            this.Btn_editar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
-            this.Btn_editar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_editar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.Btn_editar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_editar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_editar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_editar.Image")));
-            this.Btn_editar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Btn_editar.Location = new System.Drawing.Point(437, 66);
-            this.Btn_editar.Margin = new System.Windows.Forms.Padding(2);
-            this.Btn_editar.Name = "Btn_editar";
-            this.Btn_editar.Size = new System.Drawing.Size(84, 89);
-            this.Btn_editar.TabIndex = 158;
-            this.Btn_editar.Text = "Editar";
-            this.Btn_editar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Btn_editar.UseVisualStyleBackColor = false;
-            // 
-            // Btn_ingresar
-            // 
-            this.Btn_ingresar.BackColor = System.Drawing.Color.SkyBlue;
-            this.Btn_ingresar.FlatAppearance.BorderSize = 3;
-            this.Btn_ingresar.FlatAppearance.CheckedBackColor = System.Drawing.Color.White;
-            this.Btn_ingresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.Btn_ingresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
-            this.Btn_ingresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_ingresar.Image = ((System.Drawing.Image)(resources.GetObject("Btn_ingresar.Image")));
-            this.Btn_ingresar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.Btn_ingresar.Location = new System.Drawing.Point(353, 66);
-            this.Btn_ingresar.Margin = new System.Windows.Forms.Padding(2);
-            this.Btn_ingresar.Name = "Btn_ingresar";
-            this.Btn_ingresar.Size = new System.Drawing.Size(84, 89);
-            this.Btn_ingresar.TabIndex = 157;
-            this.Btn_ingresar.Text = "Ingresar";
-            this.Btn_ingresar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.Btn_ingresar.UseVisualStyleBackColor = false;
+            this.navegador2.BackColor = System.Drawing.Color.White;
+            this.navegador2.Location = new System.Drawing.Point(3, 24);
+            this.navegador2.Name = "navegador2";
+            this.navegador2.Size = new System.Drawing.Size(850, 650);
+            this.navegador2.TabIndex = 3;
             // 
             // Frm_MantenimientoDepartamentos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(125)))), ((int)(((byte)(125)))));
-            this.ClientSize = new System.Drawing.Size(1132, 556);
-            this.Controls.Add(this.Btn_consultar);
-            this.Controls.Add(this.Btn_borrar);
-            this.Controls.Add(this.Btn_guardar);
-            this.Controls.Add(this.Btn_editar);
-            this.Controls.Add(this.Btn_ingresar);
-            this.Controls.Add(this.lbl_titulo);
+            this.ClientSize = new System.Drawing.Size(859, 689);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Frm_MantenimientoDepartamentos";
             this.Text = "Frm_MantenimientoDepartamentos";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -170,10 +86,7 @@
 
         private CapaDeDiseno.Navegador navegador1;
         private System.Windows.Forms.Label lbl_titulo;
-        private System.Windows.Forms.Button Btn_consultar;
-        private System.Windows.Forms.Button Btn_borrar;
-        private System.Windows.Forms.Button Btn_guardar;
-        private System.Windows.Forms.Button Btn_editar;
-        private System.Windows.Forms.Button Btn_ingresar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private CapaDeDiseno.Navegador navegador2;
     }
 }
