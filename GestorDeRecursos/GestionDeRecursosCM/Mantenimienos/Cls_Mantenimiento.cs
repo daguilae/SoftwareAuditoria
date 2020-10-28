562388c151e82923bbdea993fc18e00d900ca992
aliaa114 <labrild@miumg.edu.gt>
2020-10-28 09:40:01 -0600

    creacion de clase de manejo de mantenimientos

---
+++ GestorDeRecursos/GestionDeRecursosCM/Mantenimienos/Cls_Mantenimiento.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionRecursosCM.Mantenimienos
+{
+    public class Cls_Mantenimientos
+    {
+        /*
+         ID DE TABLAS:
+            1 = Instrumento
+            2 = Recursos
+            3 = Tipos Recursos
+
+        ORDEN DE LOS DATOS EN RETURN PARA datos:
+            1 = alias
+            2 = ayuda
+            3 = tabla
+            4 = form
+            5 = nombre
+            6 = noForaneas
+
+        ORDEN DE LOS DATOS EN RETURN PARA foraneas:
+            1 = tabla
+            2 = campo
+            3 = modo
+         */
+
+        public (string[], string, string, string, string, int) datos(int tabla)
+        {
+            switch (tabla)
+            {
+                default:
+                    break;
+            }
+            return (null, null, null, null, null, 0);
+        }
+
+        public (string, string, int) foraneas(int tabla, int no)
+        {
+            switch (tabla)
+            {
+               
+                default:
+                    break;
+            }
+            return ("", "", 0);
+        }
+    }
+}
