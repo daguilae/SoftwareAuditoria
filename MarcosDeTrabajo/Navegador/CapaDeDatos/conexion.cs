4e71c8320157e7f902fd48952e9d93883c8d6d4a
Gus657 <ggusperezg@gmail.com>
2020-10-31 14:25:37 -0600

    Mantenimientos V2

---
+++ MarcosDeTrabajo/Navegador/CapaDeDatos/conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDatos
+{
+    class conexion
+    {
+
+        private string nombreBd { get; set; }
+
+        public conexion(string nombreBd)
+        {
+            this.nombreBd = nombreBd;
+        }
+
+        public OdbcConnection probarConexion()
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn="+nombreBd);// creacion de la conexion via ODBC
+            try
+            {              
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }       
+    }
+}
