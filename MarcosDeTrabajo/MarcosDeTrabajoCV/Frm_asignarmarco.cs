1b1757595bcdbee16f325d681c46326989131a61
Gus657 <ggusperezg@gmail.com>
2020-10-31 14:47:35 -0600

    Mantenimientos V1

--- MarcosDeTrabajo/MarcosDeTrabajoCV/Frm_asignarmarco.cs
+++ MarcosDeTrabajo/MarcosDeTrabajoCV/Frm_asignarmarco.cs
@@ -12,31 +12,14 @@ namespace MarcosDeTrabajoCV
 {
     public partial class Frm_asignarmarco : Form
     {
-        string usuario = "";
-        public Frm_asignarmarco(string user, string namebd)
+        public Frm_asignarmarco()
         {
-            InitializeComponent();            
-                        
-            usuario = user;
-            Nav_marcosdetrabajo.setBd(namebd);
-            string[] alias = { "Marco", "Empresa", "Fecha de Asignación", "Estado" };
-            Nav_marcosdetrabajo.asignarAlias(alias);
-            Nav_marcosdetrabajo.asignarSalida(this);            
-            Nav_marcosdetrabajo.asignarColorFondo(Color.White);
-            Nav_marcosdetrabajo.asignarColorFuente(Color.Black);
-            Nav_marcosdetrabajo.asignarAyuda("1");
-            Nav_marcosdetrabajo.asignarComboConTabla("marcotrabajo", "Nombre_MarcoTrabajo", 1);
-            Nav_marcosdetrabajo.asignarComboConTabla("empresa", "Nombre_Empresa", 1);
-            Nav_marcosdetrabajo.asignarTabla("marcotrabajo_has_empresa");
-            Nav_marcosdetrabajo.asignarNombreForm("");
+            InitializeComponent();
         }
 
-        private void Nav_marcosdetrabajo_Load(object sender, EventArgs e)
+        private void Lbl_titulo_Click(object sender, EventArgs e)
         {
-            string aplicacionActiva = "1";
-            Nav_marcosdetrabajo.ObtenerIdUsuario(usuario);
-            Nav_marcosdetrabajo.botonesYPermisosInicial(usuario, aplicacionActiva);
-            Nav_marcosdetrabajo.ObtenerIdAplicacion(aplicacionActiva);
+
         }
     }
 }
