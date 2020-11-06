ace4bd1c89e692ad5be67fcfc48c63e2ebffa60d
Jose <jose.ale.gt.umg@gmail.com>
2020-11-05 19:05:55 -0600

    ayudas

---
+++ GestionProyecto/GestionProyecto/GestionProyecto/Logica.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionProyecto
+{
+   public class Logica
+    {
+        Sentencias sn = new Sentencias();
+        public OdbcDataReader insertarrubrica(string sID_Rubrica, string Nombre_Rubrica, string Descripcion100_Rubrica, string ID_Instrumentos, string Descripcion75_Rubrica, string sDescripcion50_Rubrica, string sDescripcion25_Rubrica, string sDescripcion0_Rubrica, string sTotal_Rubrica, string sEstado)
+        {
+            return sn.Insertarrubrica( sID_Rubrica,  Nombre_Rubrica,  Descripcion100_Rubrica,  ID_Instrumentos,  Descripcion75_Rubrica,  sDescripcion50_Rubrica,  sDescripcion25_Rubrica,  sDescripcion0_Rubrica,  sTotal_Rubrica,  sEstado);
+        }
+        public OdbcDataReader consultaayuda(string id)
+        {
+            return sn.consultaayudas(id);
+        }
+    }
+}
---
+++ GestionProyecto/GestionProyecto/GestionProyecto/Mantenimientos/Frm_MantRubrica.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionProyecto.Mantenimientos
+{
+    public partial class Frm_MantRubrica : Form
+    {
+        int subtotal = 0;
+        Logica logic = new Logica();
+        public Frm_MantRubrica()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_MantRubrica_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dgw_rubric_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Dgw_rubric.Rows.Count-1; i++)
+            {
+                string col1 = Dgw_rubric.Rows[i].Cells[0].Value.ToString();
+                string col2 = Dgw_rubric.Rows[i].Cells[1].Value.ToString();
+                string col3 = Dgw_rubric.Rows[i].Cells[2].Value.ToString();
+                string col4 = Dgw_rubric.Rows[i].Cells[4].Value.ToString();
+                string col5 = Dgw_rubric.Rows[i].Cells[6].Value.ToString();
+                string col6 = Dgw_rubric.Rows[i].Cells[8].Value.ToString();
+                string col7 = Dgw_rubric.Rows[i].Cells[10].Value.ToString();
+               // string insert_sql = "INSERT INTO Rubrica(ID_Rubrica, Nombre_Rubrica, Descripcion100_Rubrica,ID_Instrumentos,Descripcion75_Rubrica,Descripcion50_Rubrica,Descripcion25_Rubrica,Descripcion0_Rubrica,Total_Rubrica,Estado) VALUES ('" + col1 + "','" + col2 + "','" + col3 + "','" + Txt_instrumento.Text + "','" + col4 + "','" + col5 + "','" + col6 + "','" + col7 + "','0','1')";
+                //this.getcom(insert_sql);
+                //MessageBox.Show(insert_sql);
+                logic.insertarrubrica(col1 ,col2 , col3, Txt_instrumento.Text ,  col4 , col5 ,  col6 , col7 , Convert.ToString(0),Convert.ToString(1));
+            }
+
+        }
+
+        private void Dgw_rubric_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dgw_rubric.Rows.Clear();
+            Txt_instrumento.Text = "";
+        }
+
+        private void btn_ayuda_Click(object sender, EventArgs e)
+        {
+            string ruta = "";
+            string indice = "";
+
+            OdbcDataReader mostrarEmpleado = logic.consultaayuda("5");
+            try
+            {
+                while (mostrarEmpleado.Read())
+                {
+                    ruta = mostrarEmpleado.GetString(2);
+                    indice = mostrarEmpleado.GetString(3);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
+
+            Help.ShowHelp(this, ruta, indice);
+
+        }
+    }
+}
